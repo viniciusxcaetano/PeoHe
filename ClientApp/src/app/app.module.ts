a4bd57ba665426fb8ca86a0e0c69aa57646c8804
vinicius <viniciusxcaetano@gmail.com>
2019-12-18 16:40:28 -0300

    Creating the models in angular

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -6,10 +6,10 @@ import { RouterModule } from '@angular/router';
 import { RepositoryService } from './shared/services/repository.service';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NavMenuComponent } from './ui/header/nav-menu/nav-menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { FetchDataComponent } from './components/fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
--- ClientApp/src/app/components/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/components/fetch-data/fetch-data.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { RepositoryService } from '../shared/services/repository.service';
+import { RepositoryService } from '../../shared/services/repository.service';
 
 @Component({
   selector: 'app-fetch-data',
