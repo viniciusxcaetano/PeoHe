4dbb8e8afe73b7c9c5be7041feb664b1b92af830
vinicius <viniciusxcaetano@gmail.com>
2020-01-23 18:30:16 -0300

    Model Clinic in Angular

--- Controllers/ClinicController.cs
+++ Controllers/ClinicController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Peohe.Db;
+using Peohe.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +8,29 @@ using System.Threading.Tasks;
 
 namespace Peohe.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class ClinicController : ControllerBase
     {
+        private readonly PeoheDbContext dbContext;
+
+        public ClinicController(PeoheDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet("GetClinics")]
+        public ActionResult<IEnumerable<Clinic>> GetClinics()
+        {
+            return dbContext.Clinics.Where(c => c.Deleted == null).ToList();
+        }
+        [HttpPost("CreateClinic")]
+        public ActionResult<Clinic> CreateClinic(Clinic clinic)
+        {
+            dbContext.Clinics.Add(clinic);
+            dbContext.SaveChanges();
+
+            return clinic;
+        }
     }
 }
